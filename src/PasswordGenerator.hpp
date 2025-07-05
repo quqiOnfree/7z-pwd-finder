94e903f2b7527e7888724ddf7864dfe12f2116d9
quqiOnfree <quqiOnfree@outlook.com>
2025-07-05 14:23:26 +0800

    Upgrade PasswordGenerator

---
+++ src/PasswordGenerator.hpp
@@ -0,0 +1,246 @@
+#pragma once
+
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+class PasswordGenerator {
+public:
+  enum PasswordMode : std::uint8_t {
+    Unknown = 0,
+    Number = 0b1,
+    Letter = 0b10,
+    SpecialCharacters = 0b100
+  };
+
+  PasswordGenerator(std::size_t min_length, std::size_t max_lenth,
+                    std::uint8_t password_mode)
+      : min_length_(min_length), max_lenth_(max_lenth),
+        password_mode_(password_mode), current_lenth_(min_length),
+        is_first(false) {
+    generatePasswordSet();
+    generateDefaultPassword();
+  }
+
+  PasswordGenerator(std::size_t min_length, std::size_t max_lenth,
+                    std::uint8_t password_mode,
+                    const std::vector<std::size_t> &current_password,
+                    const std::vector<std::size_t> &end_password)
+      : min_length_(min_length), max_lenth_(max_lenth),
+        password_mode_(password_mode), current_password_(current_password),
+        end_password_(end_password), current_lenth_(min_length),
+        is_first(false) {
+    generatePasswordSet();
+  }
+
+  PasswordGenerator(std::size_t min_length, std::uint8_t password_mode,
+                    const std::vector<std::size_t> &current_password,
+                    std::size_t count)
+      : min_length_(min_length), password_mode_(password_mode),
+        current_password_(current_password), current_lenth_(min_length),
+        is_first(false) {
+    generatePasswordSet();
+    auto local_password = current_password_;
+    std::size_t bit = 0;
+    std::size_t temp = count;
+    bool need_to_carry = false;
+    while (temp) {
+      std::size_t need_to_be_added = temp % password_set_size_;
+      if (need_to_carry) {
+        ++need_to_be_added;
+        need_to_carry = false;
+      }
+      if (local_password[bit] + need_to_be_added < password_set_size_) {
+        local_password[bit] += need_to_be_added;
+      } else {
+        local_password[bit] =
+            local_password[bit] + need_to_be_added - password_set_size_;
+        need_to_carry = true;
+      }
+      temp /= password_set_size_;
+      ++bit;
+    }
+    if (need_to_carry) {
+      if (local_password.size() > bit) {
+        ++local_password[bit];
+      } else {
+        local_password.push_back(1);
+      }
+    }
+    max_lenth_ = local_password.size();
+    end_password_ = std::move(local_password);
+  }
+
+  bool isEnd() const {
+    if (current_lenth_ == max_lenth_) {
+      for (std::size_t iter = 0; iter < max_lenth_; ++iter) {
+        if (current_password_[iter] != end_password_[iter]) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  std::string getNextPassword() {
+    if (!is_first) {
+      is_first = true;
+      std::string str(current_lenth_, 0);
+      for (std::size_t iter = 0; iter < current_lenth_; ++iter) {
+        str[iter] = password_set_[current_password_[iter]];
+      }
+      return str;
+    }
+    if (isEnd()) {
+      throw std::logic_error("Current password is the maximum");
+    }
+    if (current_password_[0] + 1 < password_set_size_) {
+      ++current_password_[0];
+      std::string str(current_lenth_, 0);
+      for (std::size_t iter = 0; iter < current_lenth_; ++iter) {
+        str[iter] = password_set_[current_password_[iter]];
+      }
+      return str;
+    }
+    auto current_password = current_password_;
+    current_password[0] = 0;
+    bool need_to_carry = true;
+    std::size_t current_lenth = current_lenth_;
+    for (std::size_t iter = 1; iter < current_lenth; ++iter) {
+      if (need_to_carry) {
+        if (current_password[iter] + 1 >= password_set_size_) {
+          current_password[iter] = 0;
+        } else {
+          ++current_password[iter];
+          need_to_carry = false;
+          break;
+        }
+      }
+    }
+    if (need_to_carry) {
+      current_password.push_back(1);
+      ++current_lenth;
+    }
+    current_password_ = std::move(current_password);
+    current_lenth_ = current_lenth;
+    std::string str(current_lenth_, 0);
+    for (std::size_t iter = 0; iter < current_lenth_; ++iter) {
+      str[iter] = password_set_[current_password_[iter]];
+    }
+    return str;
+  }
+
+  std::vector<std::size_t> getNextPassword(std::size_t num) {
+    if (!is_first) {
+      is_first = true;
+      return current_password_;
+    }
+    if (isEnd()) {
+      throw std::logic_error("Current password is the maximum");
+    }
+    auto local_password = current_password_;
+    std::size_t bit = 0;
+    std::size_t temp = num;
+    bool need_to_carry = false;
+    std::size_t current_lenth = current_lenth_;
+    while (temp) {
+      std::size_t need_to_be_added = temp % password_set_size_;
+      if (need_to_carry) {
+        ++need_to_be_added;
+        need_to_carry = false;
+      }
+      if (local_password[bit] + need_to_be_added < password_set_size_) {
+        local_password[bit] += need_to_be_added;
+      } else {
+        local_password[bit] =
+            local_password[bit] + need_to_be_added - password_set_size_;
+        need_to_carry = true;
+      }
+      temp /= password_set_size_;
+      ++bit;
+    }
+    for (std::size_t iter = bit; iter < current_lenth; ++iter) {
+      if (need_to_carry) {
+        if (local_password[iter] + 1 >= password_set_size_) {
+          local_password[iter] = 0;
+        } else {
+          ++local_password[iter];
+          need_to_carry = false;
+          break;
+        }
+      }
+    }
+    if (need_to_carry) {
+      local_password.push_back(1);
+      ++current_lenth;
+    }
+    current_lenth_ = current_lenth;
+    current_password_ = std::move(local_password);
+    if (current_lenth_ > max_lenth_) {
+      current_lenth_ = max_lenth_;
+      current_password_ = end_password_;
+    }
+    return current_password_;
+  }
+
+  std::size_t getCurrentLenth() const { return current_lenth_; }
+
+private:
+  void generatePasswordSet() {
+    if (password_mode_ == Unknown) {
+      throw std::logic_error("Unknown mode");
+    }
+    if (password_mode_ & Number) {
+      password_set_.append("0123456789");
+    }
+    if (password_mode_ & Letter) {
+      password_set_.append(
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    }
+    if (password_mode_ & SpecialCharacters) {
+      password_set_.resize(password_set_.size() + 32);
+      auto iter = password_set_.end() - 32;
+      for (char i = 33; i <= 47; ++i) {
+        *(iter++) = i;
+      }
+      for (char i = 58; i <= 64; ++i) {
+        *(iter++) = i;
+      }
+      for (char i = 91; i <= 96; ++i) {
+        *(iter++) = i;
+      }
+      for (char i = 123; i <= 126; ++i) {
+        *(iter++) = i;
+      }
+    }
+    password_set_size_ = password_set_.size();
+  }
+
+  void generateDefaultPassword() {
+    if (password_mode_ == Unknown) {
+      throw std::logic_error("Unknown mode");
+    }
+    current_password_.resize(min_length_);
+    end_password_.resize(max_lenth_);
+    for (std::size_t iter = 0; iter < min_length_; ++iter) {
+      current_password_[iter] = 0;
+    }
+    for (std::size_t iter = 0; iter < max_lenth_; ++iter) {
+      end_password_[iter] = password_set_size_ - 1;
+    }
+  }
+
+  std::size_t min_length_;
+  std::size_t max_lenth_;
+  std::uint8_t password_mode_;
+  std::string password_set_;
+  std::size_t password_set_size_;
+
+  bool is_first;
+  std::size_t current_lenth_;
+  std::vector<std::size_t> current_password_;
+  std::vector<std::size_t> end_password_;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,181 +1,24 @@
-#include <atomic>
-#include <condition_variable>
-#include <filesystem>
-#include <fstream>
-#include <functional>
-#include <iostream>
-#include <memory_resource>
-#include <mutex>
-#include <queue>
-#include <stdexcept>
-#include <string>
-#include <string_view>
-#include <thread>
-#include <vector>
+#include <print>
 
-#include "../7zip/CPP/7zip/IStream.h"
-
-#define kInputBufSize ((size_t)1 << 18)
-
-namespace fs = std::filesystem;
-
-class CMultiVolumeInStream : public IInStream {
-public:
-  CMultiVolumeInStream(
-      const std::vector<fs::path> &files,
-      std::pmr::memory_resource *mr = std::pmr::get_default_resource())
-      : ref_count_(1), memory_resource_(mr), files_(files.size()),
-        files_sizes_(files.size()), file_pos_(0), loc_pos_(0), glb_pos_(0),
-        glb_size_(0) {
-    if (files.empty()) {
-      return;
-    }
-    for (std::size_t iter = 0; iter < files.size(); ++iter) {
-      if (!fs::exists(files.at(iter))) {
-        throw std::logic_error("Invalid file path");
-      }
-      std::ifstream file(files.at(iter).generic_string(),
-                         std::ios_base::binary);
-      if (!file) {
-        throw std::logic_error("Could not open the files");
-      }
-      file.seekg(0, std::ios_base::end);
-      std::int64_t local_file_size = file.tellg();
-      files_sizes_.at(iter) = local_file_size;
-      glb_size_ += local_file_size;
-      file.seekg(0, std::ios_base::beg);
-      files_.at(iter) = std::move(file);
-    }
-  }
-
-  virtual ~CMultiVolumeInStream() noexcept = default;
-
-  STDMETHOD(Seek)(Int64 offset, UInt32 seekOrigin, UInt64 *newPosition) {
-    std::int64_t loc_size = 0;
-    switch (seekOrigin) {
-    case STREAM_SEEK_SET:
-      glb_pos_ = offset;
-      for (std::size_t iter = 0; iter < files_sizes_.size(); ++iter) {
-        std::int64_t this_file_size = files_sizes_.at(iter);
-        if ((loc_size + this_file_size) > offset) {
-          file_pos_ = iter;
-          loc_pos_ = offset - loc_size;
-          return S_OK;
-        }
-        loc_size += this_file_size;
-      }
-      return E_FAIL;
-    case STREAM_SEEK_CUR:
-      if (!offset) {
-        return S_OK;
-      }
-      if (offset > 0) {
-        if (glb_pos_ + offset > glb_size_) {
-          return E_FAIL;
-        }
-        glb_pos_ += offset;
-        if (files_sizes_.at(file_pos_) <= loc_pos_ + offset) {
-          loc_pos_ = loc_pos_ + offset - files_sizes_.at(file_pos_++);
-        } else {
-          loc_pos_ += offset;
-        }
-      } else {
-        if (glb_pos_ < -offset) {
-          return E_FAIL;
-        }
-        glb_pos_ += offset;
-        if (loc_pos_ < -offset) {
-          loc_pos_ = loc_pos_ + offset + files_sizes_.at(--file_pos_);
-        } else {
-          loc_pos_ += offset;
-        }
-      }
-      return S_OK;
-    case STREAM_SEEK_END:
-      *newPosition = offset + glb_size_;
-      return S_OK;
-
-    default:
-      return STG_E_INVALIDFUNCTION;
-    }
-    return S_OK;
-  }
-
-  STDMETHOD_(ULONG, AddRef)() { return ++ref_count_; }
-
-  STDMETHOD_(ULONG, Release)() {
-    if (--ref_count_ == 0) {
-      delete this;
-      return 0;
-    }
-    return ref_count_;
-  }
-
-  STDMETHOD(QueryInterface)(REFIID iid, void **outObject) {
-    if (iid == IID_IInStream) {
-      *outObject = static_cast<IInStream *>(this);
-      AddRef();
-      return S_OK;
-    }
-    *outObject = nullptr;
-    return E_NOINTERFACE;
-  }
-
-  STDMETHOD(Read)(void *data, UInt32 size, UInt32 *processedSize) {
-    if (files_.empty()) {
-      *processedSize = 0;
-      return S_OK;
-    }
-    std::size_t remaining = size;
-    *processedSize = 0;
-    if (file_pos_ >= files_.size() - 1 &&
-        files_sizes_.at(files_sizes_.size() - 1) - 1 <= loc_pos_) {
-      return S_OK;
-    }
-    auto &file = files_.at(file_pos_);
-    if (file.eof()) {
-      file.clear();
-    }
-    file.seekg(loc_pos_);
-    std::size_t acturally_read =
-        files_sizes_.at(file_pos_) - loc_pos_ < remaining
-            ? files_sizes_.at(file_pos_) - loc_pos_
-            : remaining;
-    char *mem = (char *)memory_resource_->allocate(acturally_read + 1);
-    std::memset(mem, 0, acturally_read + 1);
-    file.read(mem, acturally_read);
-    std::memcpy(data, mem, acturally_read);
-    memory_resource_->deallocate(mem, acturally_read + 1);
-    if (acturally_read < remaining) {
-      remaining -= acturally_read;
-      glb_pos_ += acturally_read;
-      *processedSize += acturally_read;
-      loc_pos_ = 0;
-      ++file_pos_;
-      return S_OK;
-    } else {
-      loc_pos_ += remaining;
-      glb_pos_ += remaining;
-      *processedSize += remaining;
-      remaining = 0;
-      return S_OK;
-    }
-    return S_OK;
-  }
-
-private:
-  ULONG ref_count_;
-  std::pmr::memory_resource *memory_resource_;
-
-  std::vector<std::ifstream> files_;
-  std::vector<std::int64_t> files_sizes_;
-  std::size_t file_pos_;
-  std::int64_t loc_pos_;
-  std::int64_t glb_pos_;
-  std::int64_t glb_size_;
-};
+#include "CMultiVolumeInStream.hpp"
+#include "PasswordGenerator.hpp"
 
 int main() {
-  CMultiVolumeInStream s({L""});
+  // CMultiVolumeInStream s({L""});
+  PasswordGenerator pg(10, 20,
+                       PasswordGenerator::Number | PasswordGenerator::Letter |
+                           PasswordGenerator::SpecialCharacters);
+  while (!pg.isEnd()) {
+    auto current = pg.getNextPassword(10000);
+    PasswordGenerator loc_pg(10,
+                             PasswordGenerator::Number |
+                                 PasswordGenerator::Letter |
+                                 PasswordGenerator::SpecialCharacters,
+                             current, 10000);
+    while (!loc_pg.isEnd()) {
+      std::print("{}\n", loc_pg.getNextPassword());
+    }
+    break;
+  }
   return 0;
 }
